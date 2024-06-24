23da0ed4fff46c95d3791fe52cde47c89f4e8f8b
Marko Kalinić <marko@unitegenomics.com>
2024-06-25 00:55:41 +0200

    change sql test

--- configs/data/globals.sql
+++ configs/data/globals.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:946bc2de495ab910b960a5ce2b9b8c30aecf35cc2f1c790b86052657435bc802
-size 508
+oid sha256:89e125061d6fb81d630187a801660735e7e3d96dbb8c1e5590cb7d7ef6c211ca
+size 515
